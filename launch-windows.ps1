d4d76b7fbba33afd825ffed4733e5da39d21491b
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-05-05 13:25:57 -0700

    better launch script

--- launch-windows.ps1
+++ launch-windows.ps1
@@ -1,5 +1,11 @@
-dotnet restore
+# Visual Studio likes to generate invalid files here for some reason, so autonuke it
+if (Test-Path "src/Properties/launchSettings.json") {
+    rm src/Properties/launchSettings.json
+}
+# Building first is more reliable than running directly from src
 dotnet build
+# Env configuration, should be handled externally for real usage
 $Env:ASPNETCORE_ENVIRONMENT = "Development"
 $Env:ASPNETCORE_URLS = "http://*:7801"
+# Actual runner.
 dotnet src\bin\Debug\net7.0\StableUI.dll
