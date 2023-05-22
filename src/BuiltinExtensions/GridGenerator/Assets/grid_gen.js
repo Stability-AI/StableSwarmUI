e2916673ab06422f71ebf5a410bde8dd42e6d638
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-05-22 08:56:03 -0700

    gridgen: add output link

--- src/BuiltinExtensions/GridGenerator/Assets/grid_gen.js
+++ src/BuiltinExtensions/GridGenerator/Assets/grid_gen.js
@@ -35,6 +35,7 @@ function gridGen_register() {
     gridGen_settingsDiv = createDiv('grid-gen-settings-area', 'grid-gen-settings-area');
     gridGen_settingsDiv.innerHTML =
         '<button class="grid-gen-run-button" id="grid-gen-run-button">Create Grid</button><br>'
+        + '<br><div id="grid-gen-info-box"></div>'
         + makeTextInput(null, 'grid-gen-output-folder-name', 'Output Folder Name', '', '', 1, 'Output folder name...')
         + '<br>'
         + makeCheckboxInput(null, 'grid-gen-opt-do-overwrite', 'Overwrite Existing Files', 'If checked, will overwrite any already-generated images.', false)
@@ -45,6 +46,11 @@ function gridGen_register() {
         ;
     gridGen_mainDiv.appendChild(gridGen_settingsDiv);
     gridGen_mainDiv.appendChild(gridGen_axisDiv);
+    let outInfoBox = document.getElementById('grid-gen-info-box');
+    let outputFolder = document.getElementById('grid-gen-output-folder-name');
+    outputFolder.addEventListener('input', () => {
+        outInfoBox.innerHTML = `Output will be saved to <a href="Output/${outputFolder.value}/index.html" target="_blank">Output/<code>${outputFolder.value}</code></a>`;
+    });
     let runButton = document.getElementById('grid-gen-run-button');
     runButton.addEventListener('click', () => {
         let getOpt = (o) => document.getElementById('grid-gen-opt-' + o).checked;
