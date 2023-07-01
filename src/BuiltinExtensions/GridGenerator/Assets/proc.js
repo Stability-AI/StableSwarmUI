5d680fa88f8ace5c42ccfcafd080521f1c7a3691
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-06-30 21:39:38 -0700

    type check to prevent errors

--- src/BuiltinExtensions/GridGenerator/Assets/proc.js
+++ src/BuiltinExtensions/GridGenerator/Assets/proc.js
@@ -194,6 +194,9 @@ window.addEventListener('keydown', function(kbevent) {
 }, true);
 
 function escapeHtml(text) {
+    if (typeof text != 'string') {
+        return text;
+    }
     return text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
 }
 
