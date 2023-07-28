048fbb3afe29586638dc4189916db214dd50ec45
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-07-28 11:50:49 -0700

    fix sapi extension

--- src/BuiltinExtensions/StabilityAPI/StabilityAPIExtension.cs
+++ src/BuiltinExtensions/StabilityAPI/StabilityAPIExtension.cs
@@ -14,7 +14,7 @@ public class StabilityAPIExtension : Extension
     public static string[] Samplers = new[] { "DDIM", "DDPM", "K_DPMPP_2M", "K_DPMPP_2S_ANCESTRAL", "K_DPM_2", "K_DPM_2_ANCESTRAL", "K_EULER", "K_EULER_ANCESTRAL", "K_HEUN", "K_LMS" };
 
     /// <summary>Set of all feature-ids supported by StabilityAPI backends.</summary>
-    public static HashSet<string> FeaturesSupported = new();
+    public static HashSet<string> FeaturesSupported = new() { "sapi" };
 
     public static T2IRegisteredParam<string> EngineParam, SamplerParam;
 
