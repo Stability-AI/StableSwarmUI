f70d286f81fabfb1cae72dcf08a89c63f5950df0
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-08-04 14:37:13 -0700

    add protection against MS.NET missing a basic essential nuget source
    aaaaaa

--- launch-windows.bat
+++ launch-windows.bat
@@ -5,6 +5,9 @@ cd "%~dp0"
 rem Microsoft borked the dotnet installer/path handler, so force x64 to be read first
 set PATH=C:\Program Files\dotnet;%PATH%
 
+rem For some reason Microsoft's nonsense is missing the official nuget source? So forcibly add that to be safe.
+dotnet nuget add source https://api.nuget.org/v3/index.json --name "NuGet official package source"
+
 dotnet build src/StableSwarmUI.csproj --configuration Release -o src/bin/live_release
 
 set ASPNETCORE_ENVIRONMENT="Production"
