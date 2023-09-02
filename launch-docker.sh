e2e960b51a05e8c8a1a707ca7517e27e70688cfb
surfingdoggo <c@surfingdoggo.com>
2023-09-02 22:32:30 +0200

    trying dotnet's container creation tool

--- launch-docker.sh
+++ launch-docker.sh
@@ -5,11 +5,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 sed -i 's/webinstall/none/g' ./src/Core/Settings.cs
+sed -i '/<PropertyGroup>/a \
+    <ContainerBaseImage>moneymarathon/container-images:dotnet</ContainerBaseImage>' \
+    src/StableSwarmUI.csproj
 
 # Building first is more reliable than running directly from src
-dotnet build src/StableSwarmUI.csproj --configuration Release -o ./src/bin/live_release
+# dotnet build src/StableSwarmUI.csproj --configuration Release -o ./src/bin/live_release
 # Default env configuration, gets overwritten by the C# code's settings handler
 ASPNETCORE_ENVIRONMENT="Production"
 ASPNETCORE_URLS="http://*:7801"
-dotnet publish --os linux --arch x64 -c Release --property:PublishDir=/publish --self-contained true #/t:PublishContainer -c Release p:PublishReadyToRun=true 
-cp -R /code/StableSwarmUI/src /publish/src
+# dotnet publish --os linux --arch x64 -c Release --property:PublishDir=/publish --self-contained true #/t:PublishContainer -c Release p:PublishReadyToRun=true 
+# cp -R /code/StableSwarmUI/src /publish/src
