cdc49fa2540afa86a1d2b35f5a9c7cb57ad9665e
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-09-06 14:00:59 -0700

    cleaner format of setting descriptions

--- src/Core/Settings.cs
+++ src/Core/Settings.cs
@@ -27,7 +27,7 @@ public class Settings : AutoConfiguration
     [ConfigComment("Ratelimit, in milliseconds, between Nvidia GPU status queries. Default is 1000 ms (1 second).")]
     public long NvidiaQueryRateLimitMS = 1000;
 
-    [ConfigComment("How to launch the UI. If 'none', just quietly launch. If 'web', launch your web-browser to the page. If 'webinstall', launch web-browser to the install page. If 'electron', launch the UI in an electron window.")]
+    [ConfigComment("How to launch the UI. If 'none', just quietly launch.\nIf 'web', launch your web-browser to the page.\nIf 'webinstall', launch web-browser to the install page.\nIf 'electron', launch the UI in an electron window (NOT YET IMPLEMENTED).")]
     [ManualSettingsOptions(Impl = null, Vals = new string[] { "none", "web", "webinstall", "electron" })]
     public string LaunchMode = "webinstall";
 
@@ -51,71 +51,71 @@ public class Settings : AutoConfiguration
     public class NetworkData : AutoConfiguration
     {
         [ConfigComment("What web host address to use. `localhost` means your PC only."
-            + " Linux users may use `0.0.0.0` to mean accessible to anyone that can connect to your PC (ie LAN users, or the public if your firewall is open)."
-            + " Windows users may use `*` for that, though it may require additional Windows firewall configuration."
-            + " Advanced server users may wish to manually specify a host bind address here.")]
+            + "\nLinux users may use `0.0.0.0` to mean accessible to anyone that can connect to your PC (ie LAN users, or the public if your firewall is open)."
+            + "\nWindows users may use `*` for that, though it may require additional Windows firewall configuration."
+            + "\nAdvanced server users may wish to manually specify a host bind address here.")]
         public string Host = "localhost";
 
         [ConfigComment("What web port to use. Default is '7801'.")]
         public int Port = 7801;
 
-        [ConfigComment("If true, if the port is already in use, the server will try to find another port to use instead. If false, the server will fail to start if the port is already in use.")]
+        [ConfigComment("If true, if the port is already in use, the server will try to find another port to use instead.\nIf false, the server will fail to start if the port is already in use.")]
         public bool PortCanChange = true;
     }
 
     /// <summary>Settings related to file paths.</summary>
     public class PathsData : AutoConfiguration
     {
-        [ConfigComment("Root path for model files. Use a full-formed path (starting with '/' or a Windows drive like 'C:') to use an absolute path. Defaults to 'Models'.")]
+        [ConfigComment("Root path for model files. Use a full-formed path (starting with '/' or a Windows drive like 'C:') to use an absolute path.\nDefaults to 'Models'.")]
         public string ModelRoot = "Models";
 
-        [ConfigComment("The model folder to use within 'ModelRoot'. Defaults to 'Stable-Diffusion'.")]
+        [ConfigComment("The model folder to use within 'ModelRoot'.\nDefaults to 'Stable-Diffusion'.")]
         public string SDModelFolder = "Stable-Diffusion";
 
-        [ConfigComment("The LoRA (or related adapter type) model folder to use within 'ModelRoot'. Defaults to 'Lora'.")]
+        [ConfigComment("The LoRA (or related adapter type) model folder to use within 'ModelRoot'.\nDefaults to 'Lora'.")]
         public string SDLoraFolder = "Lora";
 
-        [ConfigComment("The VAE (autoencoder) model folder to use within 'ModelRoot'. Defaults to 'VAE'.")]
+        [ConfigComment("The VAE (autoencoder) model folder to use within 'ModelRoot'.\nDefaults to 'VAE'.")]
         public string SDVAEFolder = "VAE";
 
-        [ConfigComment("The Embedding (eg textual inversion) model folder to use within 'ModelRoot'. Defaults to 'Embeddings'.")]
+        [ConfigComment("The Embedding (eg textual inversion) model folder to use within 'ModelRoot'.\nDefaults to 'Embeddings'.")]
         public string SDEmbeddingFolder = "Embeddings";
 
-        [ConfigComment("The ControlNets model folder to use within 'ModelRoot'. Defaults to 'controlnet'.")]
+        [ConfigComment("The ControlNets model folder to use within 'ModelRoot'.\nDefaults to 'controlnet'.")]
         public string SDControlNetsFolder = "controlnet";
 
-        [ConfigComment("The CLIP Vision model folder to use within 'ModelRoot'. Defaults to 'clip_vision'.")]
+        [ConfigComment("The CLIP Vision model folder to use within 'ModelRoot'.\nDefaults to 'clip_vision'.")]
         public string SDClipVisionFolder = "clip_vision";
 
-        [ConfigComment("Root path for data (user configs, etc). Defaults to 'Data'")]
+        [ConfigComment("Root path for data (user configs, etc).\nDefaults to 'Data'")]
         public string DataPath = "Data";
 
-        [ConfigComment("Root path for output files (images, etc). Defaults to 'Output'")]
+        [ConfigComment("Root path for output files (images, etc).\nDefaults to 'Output'")]
         public string OutputPath = "Output";
 
-        [ConfigComment("When true, output paths always have the username as a folder. When false, this will be skipped. Keep this on in multi-user environments.")]
+        [ConfigComment("When true, output paths always have the username as a folder.\nWhen false, this will be skipped.\nKeep this on in multi-user environments.")]
         public bool AppendUserNameToOutputPath = true;
     }
 
     /// <summary>Settings to control restrictions on users.</summary>
     public class UserRestriction : AutoConfiguration
     {
-        [ConfigComment("How many directories deep a user's custom OutPath can be. Default is 5.")]
+        [ConfigComment("How many directories deep a user's custom OutPath can be.\nDefault is 5.")]
         public int MaxOutPathDepth = 5;
 
-        [ConfigComment("Which user-settings the user is allowed to modify. Default is all of them.")]
+        [ConfigComment("Which user-settings the user is allowed to modify.\nDefault is all of them.")]
         public List<string> AllowedSettings = new() { "*" };
 
-        [ConfigComment("If true, the user is treated as a full admin. This includes the ability to modify these settings.")]
+        [ConfigComment("If true, the user is treated as a full admin.\nThis includes the ability to modify these settings.")]
         public bool Admin = false;
 
-        [ConfigComment("If true, user may load models. If false, they may only use already-loaded models.")]
+        [ConfigComment("If true, user may load models.\nIf false, they may only use already-loaded models.")]
         public bool CanChangeModels = true;
 
-        [ConfigComment("What models are allowed, as a path regex. Directory-separator is always '/'. Can be '.*' for all, 'MyFolder/.*' for only within that folder, etc. Default is all.")]
+        [ConfigComment("What models are allowed, as a path regex.\nDirectory-separator is always '/'. Can be '.*' for all, 'MyFolder/.*' for only within that folder, etc.\nDefault is all.")]
         public string AllowedModels = ".*";
 
-        [ConfigComment("Generic permission flags. '*' means all. Default is all.")]
+        [ConfigComment("Generic permission flags. '*' means all.\nDefault is all.")]
         public List<string> PermissionFlags = new() { "*" };
 
         [ConfigComment("How many images can try to be generating at the same time on this user.")]
@@ -135,7 +135,7 @@ public class Settings : AutoConfiguration
             // TODO: Docs link that documents full set of options here.
             public string Format = "raw/[year]-[month]-[day]/[prompt]-[model]-[seed]";
 
-            [ConfigComment("How long any one part can be. Default is 40 characters.")]
+            [ConfigComment("How long any one part can be.\nDefault is 40 characters.")]
             public int MaxLenPerPart = 40;
         }
 
@@ -144,17 +144,17 @@ public class Settings : AutoConfiguration
 
         public class FileFormatData : AutoConfiguration
         {
-            [ConfigComment("What format to save images in. Default is '.jpg' (at 100% quality).")]
+            [ConfigComment("What format to save images in.\nDefault is '.jpg' (at 100% quality).")]
             [SettingsOptions(Impl = typeof(SettingsOptionsAttribute.ForEnum<Image.ImageFormat>))]
             public string ImageFormat = "JPG";
 
-            [ConfigComment("Whether to store metadata into saved images. Defaults enabled.")]
+            [ConfigComment("Whether to store metadata into saved images.\nDefaults enabled.")]
             public bool SaveMetadata = true;
 
-            [ConfigComment("If set to non-0, adds DPI metadata to saved images. '72' is a good value for compatibility with some external software.")]
+            [ConfigComment("If set to non-0, adds DPI metadata to saved images.\n'72' is a good value for compatibility with some external software.")]
             public int DPI = 0;
 
-            [ConfigComment("If set to true, a '.txt' file will be saved alongside images with the image metadata easily viewable. This can work even if saving in the image is disabled. Defaults disabled.")]
+            [ConfigComment("If set to true, a '.txt' file will be saved alongside images with the image metadata easily viewable.\nThis can work even if saving in the image is disabled. Defaults disabled.")]
             public bool SaveTextFileMetadata = false;
         }
 
