650a4f37599b704cd6e8b633f6558bf94a4b13de
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-09-29 21:06:35 -0700

    swap windows powershell launch to a dev-specific launch
    and the bat for normal people

--- launch-windows-dev.ps1
+++ launch-windows-dev.ps1
@@ -1,14 +1,22 @@
 # Ensure correct local path.
 $thisPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 cd $thisPath
+
 # Visual Studio likes to generate invalid files here for some reason, so autonuke it
 if (Test-Path "src/Properties/launchSettings.json") {
     rm src/Properties/launchSettings.json
 }
+
+# Nuke build files to ensure our build is fresh and won't skip past errors
+Remove-Item 'src/bin/' -Recurse
+Remove-Item 'src/obj/' -Recurse
+
 # Building first is more reliable than running directly from src
 dotnet build src/StableSwarmUI.csproj --configuration Release -o src/bin/live_release
+
 # Default env configuration, gets overwritten by the C# code's settings handler
 $Env:ASPNETCORE_ENVIRONMENT = "Production"
 $Env:ASPNETCORE_URLS = "http://*:7801"
+
 # Actual runner.
-dotnet src\bin\live_release\StableSwarmUI.dll @args
+dotnet src\bin\live_release\StableSwarmUI.dll --environment dev @args
--- launch-windows.bat
+++ launch-windows.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem Ensure correct local path.
 cd /D "%~dp0"
 
 rem Microsoft borked the dotnet installer/path handler, so force x64 to be read first
@@ -8,8 +9,10 @@ set PATH=C:\Program Files\dotnet;%PATH%
 rem For some reason Microsoft's nonsense is missing the official nuget source? So forcibly add that to be safe.
 dotnet nuget add source https://api.nuget.org/v3/index.json --name "NuGet official package source"
 
+rem Building first is more reliable than running directly from src
 dotnet build src/StableSwarmUI.csproj --configuration Release -o src/bin/live_release
 
+rem Default env configuration, gets overwritten by the C# code's settings handler
 set ASPNETCORE_ENVIRONMENT="Production"
 set ASPNETCORE_URLS="http://*:7801"
 
