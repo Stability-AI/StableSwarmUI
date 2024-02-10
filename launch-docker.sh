3e72ae57132f6b23758b11c00fc5e2c1232f9e46
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-02-10 10:18:50 -0800

    add --name and a comment to docker script for #232

--- launch-docker.sh
+++ launch-docker.sh
@@ -2,7 +2,9 @@
 
 docker build -t stableswarmui .
 
+# add "--network=host" if you want to access other services on the host network (eg a separated comfy instance)
 docker run -it \
+    --name stableswarmui \
     --mount source=swarmdata,target=/Data \
     --mount source=swarmbackend,target=/dlbackend \
     -v ./Models:/Models \
