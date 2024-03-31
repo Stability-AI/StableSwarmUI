be10a881d9b9ca005c16961a2677bc7274a1622d
Philip Brown <phil@bolthole.com>
2024-03-31 09:28:28 -0700

    Update install-linux.sh
    check for existence of venv on linux

--- install-linux.sh
+++ install-linux.sh
@@ -30,5 +30,24 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh --channel 8.0
 cd ..
 
+
+# Ensure we have python, AND venv installed
+python=`which python ||which python3`
+if [ "$python" == "" ] ; then echo ERROR: cannot find python; exit 1; fi
+#pip=`which pip`
+# Would check for pip, but things work on ubuntu22 even without a
+# visible 'pip' program
+venv=`$python -m venv 2>&1`
+case $venv in
+    *usage*)
+        :
+    ;;
+    *)
+        echo ERROR: python venv is not installed
+        echo You may need to do: sudo apt install python3-venv
+        exit 1
+    ;;
+esac
+
 # Launch
 ./launch-linux.sh $@
