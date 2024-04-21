b4a1f13ac05aba550a238360ba22dd07a43f59e2
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-04-21 07:44:38 -0700

    launch scripts: auto build if git pull happened
    Batch should be outlawed and all responsible for its existence punished. And whoever at microsoft made it so powershell requires manual enabling before you can use it should also be punished.

--- launch-linux.sh
+++ launch-linux.sh
@@ -7,9 +7,23 @@ cd $SCRIPT_DIR
 # Add dotnet non-admin-install to path
 export PATH="$SCRIPT_DIR/.dotnet:~/.dotnet:$PATH"
 
+if [ -d .git ]; then
+    cur_head=`git rev-parse HEAD`
+    built_head=`cat src/bin/last_build`
+    if [ "$cur_head" != "$built_head" ]; then
+        printf "\n\nWARNING: You did a git pull without building. Will now build for you...\n\n"
+        rm -rf ./src/bin/live_release_backup
+        mv ./src/bin/live_release ./src/bin/live_release_backup
+    fi
+else
+    printf "\n\nWARNING: YOU DID NOT CLONE FROM GIT. THIS WILL BREAK SOME SYSTEMS. PLEASE INSTALL PER THE README.\n\n"
+fi
+
 # Build the program if it isn't already built
 if [ ! -f src/bin/live_release/StableSwarmUI.dll ]; then
     dotnet build src/StableSwarmUI.csproj --configuration Release -o ./src/bin/live_release
+    cur_head=`git rev-parse HEAD`
+    echo $cur_head > src/bin/last_build
 fi
 
 # Default env configuration, gets overwritten by the C# code's settings handler
--- launch-windows.bat
+++ launch-windows.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 rem Ensure correct local path.
 cd /D "%~dp0"
@@ -6,12 +7,30 @@ cd /D "%~dp0"
 rem Microsoft borked the dotnet installer/path handler, so force x64 to be read first
 set PATH=C:\Program Files\dotnet;%PATH%
 
-rem For some reason Microsoft's nonsense is missing the official nuget source? So forcibly add that to be safe.
-dotnet nuget add source https://api.nuget.org/v3/index.json --name "NuGet official package source"
+if not exist .git (
+    echo "" & echo ""
+    echo "WARNING: YOU DID NOT CLONE FROM GIT. THIS WILL BREAK SOME SYSTEMS. PLEASE INSTALL PER THE README."
+    echo "" & echo ""
+) else (
+    for /f "delims=" %%i in ('git rev-parse HEAD') do set CUR_HEAD=%%i
+    set /p BUILT_HEAD=<src/bin/last_build
+    if not "!CUR_HEAD!"=="!BUILT_HEAD!" (
+        echo "" & echo ""
+        echo "WARNING: You did a git pull without building. Will now build for you..."
+        echo "" & echo ""
+        rmdir /s /q .\src\bin\live_release_backup
+        move .\src\bin\live_release .\src\bin\live_release_backup
+    )
+)
 
 rem Build the program if it isn't already built
 if not exist src\bin\live_release\StableSwarmUI.dll (
+    rem For some reason Microsoft's nonsense is missing the official nuget source? So forcibly add that to be safe.
+    dotnet nuget add source https://api.nuget.org/v3/index.json --name "NuGet official package source"
+
     dotnet build src/StableSwarmUI.csproj --configuration Release -o src/bin/live_release
+    for /f "delims=" %%i in ('git rev-parse HEAD') do set CUR_HEAD2=%%i
+    echo !CUR_HEAD2!> src/bin/last_build
 )
 
 rem Default env configuration, gets overwritten by the C# code's settings handler
